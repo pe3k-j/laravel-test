75375a883ac10ac9f2118d0bd2aa37a6a82a7a14
Pe3k <jakub53k@gmail.com>
2023-10-17 13:39:35 +0200

    Using Eloquent to build SQL

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 
 class IndexController extends Controller
 {
     //
     public function index()
     {
-        $view = view('index.index');
+
+        $movies = Movie::query()
+            ->where('votes_nr', '>=', 5000)
+            ->where('movie_type_id', 1)
+            ->orderBy('rating', 'desc')
+            ->limit(10)
+            ->pluck('name');
+
+        $view = view('index.index', ['movies' => $movies]);
 
         return $view;
     }
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,1 +1,9 @@
 <h1>Hoem Page</h1>
+
+<ul>
+    <?php foreach ($movies as $movie) : ?>
+        <li>
+            <?= $movie ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
